f458410c3ced6376f293931f7ee950c600ce8256
Hell0Ankit <ankitbohra660@gmail.com>
2026-06-13 16:59:45 +0530

    Added sign-UP page

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('login/', views.loginViews, name='login'),
+    path('sign-up/', views.signupViews, name='signup'),
     
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def loginViews(request):
     return render(request, 'accounts/login.html')
+
+def signupViews(request):
+    return render(request, 'accounts/signup.html')
