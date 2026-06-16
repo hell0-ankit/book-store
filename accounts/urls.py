82aff7606bbc9d1d1a27f28ef45a9f9311059f1e
Hell0Ankit <ankitbohra660@gmail.com>
2026-06-16 16:34:08 +0530

    Added forget-pasword.html and verify-otp.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,9 @@ from django.urls import path
 urlpatterns = [
     path('login/', views.loginViews, name='login'),
     path('sign-up/', views.signupViews, name='signup'),
+    path('forget-passward/', views.forgetViews, name='forgetpassward'),
+    path('verify-otp/', views.verifyOTPViews, name='verify_otp'),
+    path("resend-otp/", views.resend_otp, name="resend_otp"),
+    path("reset-password/", views.reset_password, name="reset_password"),
     
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def loginViews(request):
@@ -6,3 +6,15 @@ def loginViews(request):
 
 def signupViews(request):
     return render(request, 'accounts/signup.html')
+
+def forgetViews(request):
+    return render(request, 'accounts/forget-password.html')
+
+def verifyOTPViews(request):
+    return render(request, 'accounts/verify-otp.html')
+
+def resend_otp(request):
+    return redirect("verify_otp")
+
+def reset_password(request):
+    return render(request, 'accounts/verify-otp.html')
