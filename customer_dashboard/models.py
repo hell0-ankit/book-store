7d7b52f976b87ab0432f3cfc1a9a495b6ba21ab6
Hell0Ankit <ankitbohra660@gmail.com>
2026-06-24 21:17:39 +0530

    Complete customer_dashboard Design

--- customer_dashboard/models.py
+++ customer_dashboard/models.py
@@ -7,6 +7,7 @@ class CustomerStatus(models.TextChoices):
     INACTIVE = "INACTIVE", "Inactive"
     PENDING = "PENDING", "Pending"
     BLOCKED = "BLOCKED", "Blocked"
+    
 class Gender(models.TextChoices):
     MALE = "MALE", "Male"
     FEMALE = "FEMALE", "Female"
@@ -16,11 +17,11 @@ class Gender(models.TextChoices):
 class PersonalDetils(models.Model):
     user_details=models.OneToOneField(User, on_delete=models.CASCADE)
     cm_phone = models.CharField(unique=True, max_length=50)
-    cm_status = models.CharField(max_length=20, choices=CustomerStatus.choices, default=CustomerStatus.PENDING)
     cm_gender = models.CharField(max_length=20, choices=Gender.choices)
     cm_dob = models.TimeField(auto_now=False, auto_now_add=False)
     address = models.TextField(blank=True)
     profile_image = models.ImageField(upload_to='userprofile/')
+    cm_status = models.CharField(max_length=20, choices=CustomerStatus.choices, default=CustomerStatus.PENDING)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     def __str__(self):
