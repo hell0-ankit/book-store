bee039a52d598cde2bbdd2553e493bf15b02a120
Hell0Ankit <ankitbohra660@gmail.com>
2026-07-09 22:33:30 +0530

    fix(cart): display total quantity instead of unique products

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -1,11 +1,21 @@
 from cart.models import Cart, CartItem
+from django.db.models import Sum
 
 def cart_items(request):
     cart_items= CartItem.objects.all()
     cart_item = CartItem.objects.first()
+    if request.user.is_authenticated:
+        total_quantity = (
+            CartItem.objects.filter(cart__user=request.user)
+            .aggregate(total=Sum("quantity"))["total"] or 0
+        )
+    else:
+        total_quantity = 0
+    print("count items : ", total_quantity)
     return {
         "cart_items": cart_items,
-        "cart_item":cart_item
+        "cart_item":cart_item,
+        "total_quantity":total_quantity
     }
 
 def cart(request):
